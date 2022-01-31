b010f4282dd44258762182fd2c5f68383bf410e9
Nguyễn Đức Mạnh <manhnd.jin@gmail.com>
2022-01-31 20:32:41 +0700

    init focus app

---
+++ apps/focus/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'focus',
+  webDir: '../../dist/apps/focus',
+  bundledWebRuntime: false,
+};
+
+export default config;
